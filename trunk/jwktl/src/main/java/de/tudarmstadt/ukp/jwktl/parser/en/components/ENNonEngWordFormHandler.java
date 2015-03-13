32cb9bd6461b6960ee7244e44668b49ca3220f84
jan.berkel@gmail.com <jan.berkel@gmail.com@16df82f2-aba5-e49c-8055-7a5891c53f76>
2015-03-13 13:01:55 +0000

    Convert spaces to tabs for consistency

--- trunk/jwktl/src/main/java/de/tudarmstadt/ukp/jwktl/parser/en/components/ENNonEngWordFormHandler.java
+++ trunk/jwktl/src/main/java/de/tudarmstadt/ukp/jwktl/parser/en/components/ENNonEngWordFormHandler.java
@@ -31,73 +31,73 @@ import de.tudarmstadt.ukp.jwktl.api.util.TemplateParser.Template;
  * Support for parsing word forms for non-English entries in the English Wiktionary.
  */
 public class ENNonEngWordFormHandler implements IWordFormHandler, TemplateParser.ITemplateHandler {
-    private static final Pattern HEAD_PATTERN = Pattern.compile("\\A\\{\\{head|");
-    private static final Pattern NOUN_PATTERN = Pattern.compile("\\A\\{\\{(\\w+)\\-noun");
-    private GrammaticalGender gender;
+	private static final Pattern HEAD_PATTERN = Pattern.compile("\\A\\{\\{head|");
+	private static final Pattern NOUN_PATTERN = Pattern.compile("\\A\\{\\{(\\w+)\\-noun");
+	private GrammaticalGender gender;
 
-    @Override
-    public boolean parse(String line) {
-        if (HEAD_PATTERN.matcher(line).find() || NOUN_PATTERN.matcher(line).find()) {
-            TemplateParser.parse(line, this);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@Override
+	public boolean parse(String line) {
+		if (HEAD_PATTERN.matcher(line).find() || NOUN_PATTERN.matcher(line).find()) {
+			TemplateParser.parse(line, this);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    @Override
-    public List<IWiktionaryWordForm> getWordForms() {
-        return Collections.emptyList();
-    }
+	@Override
+	public List<IWiktionaryWordForm> getWordForms() {
+		return Collections.emptyList();
+	}
 
-    @Override
-    public GrammaticalGender getGender() {
-        return gender;
-    }
+	@Override
+	public GrammaticalGender getGender() {
+		return gender;
+	}
 
-    @Override
-    public String handle(Template template) {
-        if (template.getName().endsWith("-noun")) {
-            handleGenericNounTemplate(template);
-        } else if (template.getName().equals("g")) {
-            handleGenericNounTemplate(template);
-        } else if (template.getName().equals("head")) {
-            handleHeadwordTemplate(template);
-        }
-        return null;
-    }
+	@Override
+	public String handle(Template template) {
+		if (template.getName().endsWith("-noun")) {
+			handleGenericNounTemplate(template);
+		} else if (template.getName().equals("g")) {
+			handleGenericNounTemplate(template);
+		} else if (template.getName().equals("head")) {
+			handleHeadwordTemplate(template);
+		}
+		return null;
+	}
 
-    // pt-noun: m, f, mf, m-f, m-p, f-p, m-f-p, morf (deprecated in favour of m|g2=f), and ?. g2=The second gender, to be used when different people use the same word with different genders
-    // fr-noun: m, f, m-p or f-p. It also accepts mf as a shortcut for m|g2=f. g2= The second gender, if any.
-    // es-noun: m, f, m-p, f-p or mf
-    // de-noun: Use m, f, n for neuter. To specify more than one gender, use g2= or g3=
-    private void handleGenericNounTemplate(Template template) {
-        if (template.getNumberedParamsCount() > 0) {
-            String genderParam = template.getNumberedParam(0);
-            gender = extractGender(genderParam);
-        }
-    }
+	// pt-noun: m, f, mf, m-f, m-p, f-p, m-f-p, morf (deprecated in favour of m|g2=f), and ?. g2=The second gender, to be used when different people use the same word with different genders
+	// fr-noun: m, f, m-p or f-p. It also accepts mf as a shortcut for m|g2=f. g2= The second gender, if any.
+	// es-noun: m, f, m-p, f-p or mf
+	// de-noun: Use m, f, n for neuter. To specify more than one gender, use g2= or g3=
+	private void handleGenericNounTemplate(Template template) {
+		if (template.getNumberedParamsCount() > 0) {
+			String genderParam = template.getNumberedParam(0);
+			gender = extractGender(genderParam);
+		}
+	}
 
-    // g= and g2=, g3=...
-    // m, m-p, m-an-p, f-d, m-p, m-p etc.
-    private void handleHeadwordTemplate(Template template) {
-        final String genderParam = template.getNamedParam("g");
-        gender = extractGender(genderParam);
-    }
+	// g= and g2=, g3=...
+	// m, m-p, m-an-p, f-d, m-p, m-p etc.
+	private void handleHeadwordTemplate(Template template) {
+		final String genderParam = template.getNamedParam("g");
+		gender = extractGender(genderParam);
+	}
 
-    private static GrammaticalGender extractGender(String genderParam) {
-        if (genderParam == null) {
-            return null;
-        }
-        // TODO: properly parse gender combinations
-        GrammaticalGender gender = null;
-        if ("m".equals(genderParam) || genderParam.startsWith("m")) {
-            gender = GrammaticalGender.MASCULINE;
-        } else if ("f".equals(genderParam) || genderParam.startsWith("f")) {
-            gender = GrammaticalGender.FEMININE;
-        } else if ("n".equals(genderParam)) {
-            gender = GrammaticalGender.NEUTER;
-        }
-        return gender;
-    }
+	private static GrammaticalGender extractGender(String genderParam) {
+		if (genderParam == null) {
+			return null;
+		}
+		// TODO: properly parse gender combinations
+		GrammaticalGender gender = null;
+		if ("m".equals(genderParam) || genderParam.startsWith("m")) {
+			gender = GrammaticalGender.MASCULINE;
+		} else if ("f".equals(genderParam) || genderParam.startsWith("f")) {
+			gender = GrammaticalGender.FEMININE;
+		} else if ("n".equals(genderParam)) {
+			gender = GrammaticalGender.NEUTER;
+		}
+		return gender;
+	}
 }
