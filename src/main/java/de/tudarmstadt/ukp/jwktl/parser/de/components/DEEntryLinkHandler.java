25768139b4bfdefa53b6685f2898c6e94818e592
chmeyer <chmeyer.de@gmail.com>
2015-08-16 13:42:00 +0200

    Fixed: Missing entry link templates for German forms (closes #21). Updated change log.

--- src/main/java/de/tudarmstadt/ukp/jwktl/parser/de/components/DEEntryLinkHandler.java
+++ src/main/java/de/tudarmstadt/ukp/jwktl/parser/de/components/DEEntryLinkHandler.java
@@ -2,13 +2,13 @@
  * Copyright 2013
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,45 +23,47 @@ import de.tudarmstadt.ukp.jwktl.parser.util.ParsingContext;
 /**
  * Parser component for extracting entry links from the German Wiktionary.
  * Entry links are defined to refer to the information of another dictionary
- * article (e.g., for alternative spellings). 
- * @author Christian M. Meyer
+ * article (e.g., for alternative spellings).
  */
 public class DEEntryLinkHandler extends DEBlockHandler {
 
 	static final String[] ENTRY_LINK_TEMPLATES = new String[]{
 		"Lemmaverweis",
 		"Grundformverweis",
+		"Grundformverweis Dekl",
+		"Grundformverweis Konj",
 		"Alte Schreibweise",
 		"Schweizer und Liechtensteiner Schreibweise"
 	};
-	
+
 	protected String entryLink;
 	protected String entryLinkType;
-	
+
 	/** Initalizes the handler. */
 	public DEEntryLinkHandler() {
 		entryLink = null;
 	}
 
+	@Override
 	public boolean canHandle(final String blockHeader) {
 		if (blockHeader == null || blockHeader.isEmpty())
 			return false;
 		if (!blockHeader.startsWith("{{"))
 			return false;
-			
+
 		for (int i = 0; i < ENTRY_LINK_TEMPLATES.length; i++) {
 			String entryLinkTemplate = "{{" + ENTRY_LINK_TEMPLATES[i] + "|";
 			if (blockHeader.startsWith(entryLinkTemplate)) {
-				entryLink = blockHeader.substring(entryLinkTemplate.length(), 
+				entryLink = blockHeader.substring(entryLinkTemplate.length(),
 						blockHeader.length() - 2);
 				int idx = entryLink.indexOf('|');
 				if (idx >= 0)
 					entryLink = entryLink.substring(0, idx);
-				entryLinkType = ENTRY_LINK_TEMPLATES[i];				
+				entryLinkType = ENTRY_LINK_TEMPLATES[i];
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
--- src/test/java/de/tudarmstadt/ukp/jwktl/parser/de/DEEntryLinkHandlerTest.java
+++ src/test/java/de/tudarmstadt/ukp/jwktl/parser/de/DEEntryLinkHandlerTest.java
@@ -2,13 +2,13 @@
  * Copyright 2013
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,6 @@ import de.tudarmstadt.ukp.jwktl.parser.de.components.DEEntryLinkHandler;
 
 /**
  * Test case for {@link DEEntryLinkHandler}.
- * @author Christian M. Meyer
  */
 public class DEEntryLinkHandlerTest extends DEWiktionaryEntryParserTest {
 
@@ -33,5 +32,12 @@ public class DEEntryLinkHandlerTest extends DEWiktionaryEntryParserTest {
 		IWiktionaryEntry entry = page.getEntry(0);
 		assertEquals("Abschluss", entry.getEntryLink());
 	}
-	
+
+	/***/
+	public void testEingaben() throws Exception {
+		IWiktionaryPage page = parse("Eingaben.txt");
+		IWiktionaryEntry entry = page.getEntry(0);
+		assertEquals("Eingabe", entry.getEntryLink());
+	}
+
 }
